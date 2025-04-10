658965340ee320c0a943a7d5bc7676f763a425b7
vocongdat <vo.cong.dat.fpt@gmail.com>
2025-04-10 07:54:04 +0700

    feat: center body and fix some bug

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,15 @@
-    const toggleBtn = document.querySelector('.toggle-menu');
-    
-    const overlay = document.querySelector('.menu-overlay');
-    
-    const drawer = document.querySelector('.menu-drawer');
+const toggleBtn = document.querySelector('.toggle-menu');
 
-    toggleBtn.addEventListener('click', () => {
-        overlay.classList.add('show');
-        drawer.classList.add('show');
-    });
+const overlay = document.querySelector('.menu-overlay');
 
-    overlay.addEventListener('click', () => {
-        overlay.classList.remove('show');
-        drawer.classList.remove('show');
-    });
+const drawer = document.querySelector('.menu-drawer');
+
+toggleBtn.addEventListener('click', () => {
+  overlay.classList.add('show');
+  drawer.classList.add('show');
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  drawer.classList.remove('show');
+});
