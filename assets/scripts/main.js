384388bedc8c0f28be32ee18bde39880ec503484
duyphucnguyen <nguyenphuc1995spc@gmail.com>
2025-04-15 19:52:45 +0700

    feature: add ui content (#5)
    * feature: add ui content
    
    * fix image path
    
    * feat: center body and fix some bug
    
    * Edit  ui content
    
    * add js in xs mobile
    
    ---------
    
    Co-authored-by: Phucnddev <duyphucnguyendell@gmail.com>
    Co-authored-by: vocongdat <vo.cong.dat.fpt@gmail.com>

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,17 @@
-    const toggleBtn = document.querySelector('.toggle-menu');
-    
-    const overlay = document.querySelector('.menu-overlay');
-    
-    const drawer = document.querySelector('.menu-drawer');
 
-    toggleBtn.addEventListener('click', () => {
-        overlay.classList.add('show');
-        drawer.classList.add('show');
-    });
+const toggleBtn = document.querySelector('.toggle-menu');
 
-    overlay.addEventListener('click', () => {
-        overlay.classList.remove('show');
-        drawer.classList.remove('show');
-    });
+const overlay = document.querySelector('.menu-overlay');
+
+const drawer = document.querySelector('.menu-drawer');
+
+toggleBtn.addEventListener('click', () => {
+
+  overlay.classList.add('show');
+  drawer.classList.add('show');
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  drawer.classList.remove('show');
+});
